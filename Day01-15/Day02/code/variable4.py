dad5863bbb8f27d2d4f1f95112472e51a7a3314e
leshphon <34265799+leshphon@users.noreply.github.com>
2019-07-02 16:16:53 +0800

    add some test value with different types in Day two

--- Day01-15/Day02/code/variable4.py
+++ Day01-15/Day02/code/variable4.py
@@ -5,6 +5,7 @@ Version: 0.1
 Author: 骆昊
 Date: 2018-02-27
 """
+from functools import reduce
 
 a = 100
 b = 1000000000000000000
@@ -13,6 +14,13 @@ d = 1 + 5j
 e = 'A'
 f = 'hello, world'
 g = True
+h = {
+    'name': 'Tars',
+    'age': 27,
+    4: 'hello'
+}
+i = [2, 5, 7, 'string']
+j = (2, 5, 7, 'string')
 print(type(a))
 print(type(b))
 print(type(c))
@@ -20,3 +28,10 @@ print(type(d))
 print(type(e))
 print(type(f))
 print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(h[4])
+
+sum_num = reduce(lambda x, y: x+y, [1,2,3,4,5])
+print(sum_num)
